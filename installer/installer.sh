f65a40916f484e29d0ebf589a18a6bb59ebd4372
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-06-27 16:37:11 +0300

    Add xcms_version_css to installer shell module

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,22 +1,47 @@
 #!/usr/bin/env bash
 
+# Commonly used installer routines
+# Author: Mikhail Veltishchev <dichlofos-mv@yandex.ru>
+
+# Please obey PEP8:
+# Exactly 2 lines between functions
+# Variable names are with_underscores
+
+
 function print_error() {
+    # Error message printer
     echo -e "\033[31;1mError:\x1b[0m $@"
 }
 
+
 function print_warning() {
+    # Warning message printer
     echo -e "\033[33mWarning:\x1b[0m $@"
 }
 
+
 function print_message() {
+    # Generic info message printer
     echo -en "[\033[32m"
     printf "%10s" "installer"
     echo -e "\x1b[0m] $@"
 }
 
+
 function fix_hgrc() {
-    hgrc="$1/.hg/hgrc"
+    repo_path="$1"
     repo_name="$2"
+    if [ -z "$repo_path" ] ; then
+        print_error "Usage: fix_hgrc <repo_path> <repo_name>"
+        return 1
+    fi
+
+    if [ -z "$repo_name" ] ; then
+        print_error "Usage: fix_hgrc <repo_path> <repo_name>"
+        return 1
+    fi
+
+    hgrc="$repo_path/.hg/hgrc"
     cat >$hgrc <<EOF
 [paths]
 default = /home/mvel/work/$repo_name
@@ -26,7 +51,35 @@ bb = ssh://hg@bitbucket.org/dichlofos/$repo_name
 [ui]
 username = Mikhail Veltishchev <dichlofos-mv@yandex.ru>
 EOF
+    cat $hgrc
 }
+
+
+function xcms_version_css() {
+    # Creates a copy of CSS files under directory named as site version.
+    # Usage: xcms_version_css <destination_dir> <relatiive_path>
+    # e.g. xcms_version_css /var/www/site_root engine
+
+    destination_dir="$1"
+    css_root_dir="$destination_dir/$2"
+    if ! [ -d "$css_root_dir" ] ; then
+        return 0
+    fi
+
+    version="$( cat $destination_dir/version | sed -e 's/[^0-9.]//g' )"
+    print_message "    Processing '$css_root_dir'..."
+
+    (
+        cd "$css_root_dir"
+        css_dirs="$( find . -type d -name 'css' )"
+        for d in $css_dirs ; do (
+            sudo rm -rf "$d/$version"
+            cd "$d"
+            sudo ln -sf . "$version"
+        ) done
+    )
+}
+
 
 function deploy_service() {
     service_name="$1"
